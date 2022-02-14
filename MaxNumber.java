9352b457be0c39784ceba781fc0c170dcacd3dbd
Austin <austinarends7@gmail.com>
2022-02-14 11:18:56 -0800

    Assignment 2.1

---
+++ MaxNumber.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+/**
+ * MaxNUmber
+ */
+public class MaxNumber {
+    public static void main( String args[] ) {
+        Random rand = new Random(); 
+
+        int numbers[] = new int[100];
+        int largest = Integer.MIN_VALUE;
+
+        for (int x = 1; x < 4; x++) {
+          int num1 = rand.nextInt(100);
+          int num2 = rand.nextInt(100);
+          int num3 = rand.nextInt(100);
+          System.out.println (rand.nextInt(100) + 1);
+
+          
+       
+          }
+        }
+      }
+    
+    
+
+    
